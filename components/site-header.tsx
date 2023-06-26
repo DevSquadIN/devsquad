a422353bafaaf60ac382a15064cd7b64458a9e98
Joy Poddar <joypoddar.sd@gmail.com>
2023-06-26 22:02:33 +0530

    feat: secondary navbar for dashboard

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,9 +1,13 @@
 import MainNav from "./navbar/main-nav"
+import SecondaryNav from "./navbar/secondary-nav"
 
 const SiteHeader = () => {
   return (
     <header>
+      {/* Conditionally renders on /index and other pages */}
       <MainNav />
+      {/* Only present in /dashboard */}
+      <SecondaryNav />
     </header>
   )
 }
