ba577852237a99d85a0796de648f08d10f798ba2
Joy Poddar <joypoddar.sd@gmail.com>
2023-07-04 09:02:29 +0530

    feat: lessons overview initial str

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,7 +1,12 @@
 import {
   Bell,
+  Check,
+  ChevronsRight,
+  LightbulbIcon,
+  LockIcon,
   LucideProps,
   UserCircle2,
+  Users2,
   type Icon as LucideIcon,
 } from "lucide-react"
 
@@ -51,4 +56,27 @@ export const SecondaryNavIcons = {
       ></path>
     </svg>
   ),
+}
+
+export const GeneralIcons = {
+  users: Users2,
+  lightBulb: LightbulbIcon,
+  lock: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+      />
+    </svg>
+  ),
+  check: Check,
+  chevronsRight: ChevronsRight,
 }
